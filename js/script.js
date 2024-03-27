101adcb572d9e2c2e1e3261861a0453f20e73403
anto-iorfida <antonioiorfida@hotmail.com>
2024-03-27 16:51:35 +0100

    feat add implemented activeImage

--- js/script.js
+++ js/script.js
@@ -4,29 +4,6 @@
 // 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
 // 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 
-const slides = [
-{
-    image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
 
 
 
@@ -37,7 +14,7 @@ createApp({
         return {
             slides : [
                 {
-                    image: 'img/01.webp',
+                        image: 'img/01.webp',
                         title: 'Marvel\'s Spiderman Miles Morale',
                         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                     }, {
@@ -58,7 +35,7 @@ createApp({
                         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                     }
                 ],
-                activeImage: 1
+                activeImage: 0
                                 
         };
     },
@@ -66,7 +43,7 @@ createApp({
        
          // Show next image
     showNextImage: function() {
-        if(this.activeImage < this.movie.images.length - 1) {
+        if(this.activeImage < this.slides.length - 1) {
           this.activeImage++;
         } else {
           this.activeImage = 0;
@@ -77,7 +54,7 @@ createApp({
         if(this.activeImage > 0) {
           this.activeImage--;
         } else {
-          this.activeImage = this.movie.images.length - 1;
+          this.activeImage = this.slides.length - 1;
         }
       }
     }
